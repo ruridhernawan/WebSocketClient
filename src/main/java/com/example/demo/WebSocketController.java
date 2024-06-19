d65edc07c628f2fcd6fb50e2f34fcf2751d08819
rurid <rurid.hernawan@agratek.id>
2024-06-19 15:24:01 +0700

    Source Langsung

--- src/main/java/com/example/demo/WebSocketController.java
+++ src/main/java/com/example/demo/WebSocketController.java
@@ -10,29 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 @Component
 public class WebSocketController {
 
-    private final MyWebSocketHandler webSocketHandler;
-    @Autowired
-    private SSHLogMonitorService sshLogMonitorService;
+	private final MyWebSocketHandler webSocketHandler;
+	@Autowired
+	private SSHLogMonitorService sshLogMonitorService;
 
-    public WebSocketController(MyWebSocketHandler webSocketHandler) {
-        this.webSocketHandler = webSocketHandler;
-    }
+	public WebSocketController(MyWebSocketHandler webSocketHandler) {
+		this.webSocketHandler = webSocketHandler;
+	}
 
-    @GetMapping("/send-message")
-    public String sendMessageToWebSocket() {
-        try {
-            // Send message to WebSocket server
-            String message = """
-                    {"user" : "live_agratek"}
-                     """;
-            webSocketHandler.sendMessage(message);
-            System.out.println("--------------->>>>>>>>>>>>>>>>>>> websocket connect");
-            sshLogMonitorService.startMonitoring();
-            System.out.println("Start Monitoring log Sucess");
-            return "Message sent to WebSocket server";
-        }
-        catch (Exception e) {
-            return "Server Contoller is useless";
-        }
-    }
+	@GetMapping("/send-message")
+	public String sendMessageToWebSocket() {
+		try {
+			// Send message to WebSocket server
+			String message = """
+					{"user" : "live_agratek"}
+					 """;
+			webSocketHandler.sendMessage(message);
+			System.out.println("--------------->>>>>>>>>>>>>>>>>>> websocket connect");
+			sshLogMonitorService.startMonitoring();
+			System.out.println("Start Monitoring log Sucess");
+			return "Message sent to WebSocket server";
+		} catch (Exception e) {
+			return "Server Contoller is useless";
+		}
+	}
 }
